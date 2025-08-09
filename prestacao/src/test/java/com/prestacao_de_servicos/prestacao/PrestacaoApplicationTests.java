985edc94f7bb9dcafda9575864ecfc76994f6516
Mateus <g77mgeem@gmail.com>
2025-08-09 18:58:05 -0300

    Primeiro commit

---
+++ prestacao/src/test/java/com/prestacao_de_servicos/prestacao/PrestacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prestacao_de_servicos.prestacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
